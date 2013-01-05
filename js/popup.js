bf25e144d1109d00f1d4cc5b31b4740bc87be587
fragallegos <fragallegos@me.com>
2013-01-04 20:09:57 -0500

    monchis web

---
+++ js/popup.js
@@ -0,0 +1,89 @@
+//SETTING UP OUR POPUP
+//0 means disabled; 1 means enabled;
+var popupStatus = 0;
+var currentActor;
+//loading popup with jQuery magic!
+function loadPopup(){
+	//loads popup only if it is disabled
+	if(popupStatus==0){
+		$("#backgroundPopup").css({
+			"opacity": "0.7"
+		});
+		$("#backgroundPopup").fadeIn("slow");
+		$("#popupContact").fadeIn("slow");
+		popupStatus = 1;
+	}
+}
+
+//disabling popup with jQuery magic!
+function disablePopup(){
+	//disables popup only if it is enabled
+	if(popupStatus==1){
+		$("#backgroundPopup").fadeOut("slow");
+		$("#popupContact").fadeOut("slow");
+		popupStatus = 0;
+		currentActor.remove();
+	}
+}
+
+//centering popup
+function centerPopup(){
+	//request data for centering
+	var windowWidth = document.documentElement.clientWidth;
+	var windowHeight = document.documentElement.clientHeight;
+	var popupHeight = $("#popupContact").height();
+	var popupWidth = $("#popupContact").width();
+	//centering
+	$("#popupContact").css({
+		"position": "absolute",
+		"top": windowHeight/2-popupHeight/2,
+		"left": windowWidth/2-popupWidth/2
+	});
+	//only need force for IE6
+	
+	$("#backgroundPopup").css({
+		"height": windowHeight
+	});
+	
+}
+
+
+//CONTROLLING EVENTS IN jQuery
+$(document).ready(function(){
+
+	//LOADING POPUP
+	//Click the button event!
+	
+		$("#cupon1").click(function(){
+			currentActor = $(
+
+							);
+			$("#popupContact").append(currentActor);		
+			start();
+		});
+
+	
+		function start(){
+			//centering with css
+			centerPopup();
+			//load popup
+			loadPopup();
+		}
+				
+	//CLOSING POPUP
+	//Click the x event!
+	$("#popupContactClose").click(function(){
+		disablePopup();
+	});
+	//Click out event!
+	$("#backgroundPopup").click(function(){
+		disablePopup();
+	});
+	//Press Escape event!
+	$(document).keypress(function(e){
+		if(e.keyCode==27 && popupStatus==1){
+			disablePopup();
+		}
+	});
+
+});
